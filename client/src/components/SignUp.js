3c475e646adc369641cb6316982219e7afc57b4e
Glendon Thaiw <glendon888@gmail.com>
2020-06-25 00:05:19 +0800

    Started basic FE form

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,7 +1,56 @@
 import React, { Component } from "react";
+import { reduxForm, Field } from "redux-form";
+import CustomInput from "./CustomInput";
 
-export default class SignUp extends Component {
+class SignUp extends Component {
+  onSubmit(data) {
+    console.log("Form Data", data);
+  }
   render() {
-    return <div>This is a Sign Up Component </div>;
+    const { handleSubmit } = this.props;
+    return (
+      <div className="row">
+        <div className="col">
+          <form onSubmit={handleSubmit(this.onSubmit)}>
+            <fieldset>
+              <Field
+                name="email"
+                type="text"
+                id="email"
+                label="Enter your email"
+                placeholder="example@gmail.com"
+                component={CustomInput}
+              />
+            </fieldset>
+            <fieldset>
+              <Field
+                d
+                name="password"
+                type="password"
+                id="password"
+                label="Enter your password"
+                placeholder="password"
+                component={CustomInput}
+              />
+            </fieldset>{" "}
+            <button type="submit" className="btn btn-primary">
+              {" "}
+              Sign Up{" "}
+            </button>
+          </form>
+        </div>
+        <div className="col">
+          <div className="text-center">
+            <div className="alert alert-primary">
+              Or Sign up using third-party services
+            </div>
+            <button className="btn btn-primary"> Facebook </button>
+            <button className="btn btn-primary"> Google </button>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
+
+export default reduxForm({ form: "signup" })(SignUp);
--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route } from "react-router-dom";
-
-// import registerServiceWorker from "./ServiceWorker";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
 import App from "./components/App";
 import Home from "./components/Home";
 import SignUp from "./components/SignUp";
 import SignIn from "./components/SignIn";
 import Dashboard from "./components/Dashboard";
+import reducers from "./reducers/index";
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/signup" component={SignUp} />
-      <Route exact path="/signin" component={SignIn} />
-      <Route exact path="/dashboard" component={Dashboard} />
-    </App>
-  </BrowserRouter>,
+  <Provider store={createStore(reducers, {})}>
+    <BrowserRouter>
+      <App>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/signin" component={SignIn} />
+        <Route exact path="/dashboard" component={Dashboard} />
+      </App>
+    </BrowserRouter>
+  </Provider>,
   document.querySelector("#root")
 );
-// registerServiceWorker();
